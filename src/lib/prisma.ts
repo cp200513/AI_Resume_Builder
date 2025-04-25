20c42d8d35ab3b6f35fd2ebc6b24c00f416675ad
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-04-25 22:45:55 +0530

    Prisma Added

---
+++ src/lib/prisma.ts
@@ -0,0 +1,18 @@
+//This code can be found in the documentation -- Prisma Next Js
+//This code is neccessary so that whenever we reload in development mode it doesn't create a new prisma client everytime
+
+import { PrismaClient } from "@prisma/client";
+
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
