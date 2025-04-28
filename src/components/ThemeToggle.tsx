161a03e37688e7743f1ceec25255bff05a45ae35
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-04-29 00:35:12 +0530

    minor twaeks

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -17,13 +17,8 @@ const ThemeToggle = () => {
   };
 
   return (
-    <Button
-      variant="ghost"
-      size="icon"
-      className="relative rounded-full"
-      onClick={onThemeToggleClick}
-    >
-      <Sun className="size-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+    <Button variant="outline" size="icon" onClick={onThemeToggleClick}>
+      <Sun className="size-[1.2rem] scale-100 rotate-0 transform transition-all dark:scale-0 dark:-rotate-90" />
       <Moon className="absolute size-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
     </Button>
   );
